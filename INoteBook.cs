83848c267c46b341e449dc021da82db32c29812c
hjdfkm <48893184+hjdfkm@users.noreply.github.com>
2019-04-09 00:35:31 +0300

    Add files via upload

---
+++ INoteBook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    interface INoteBookСompound<in T>
+    {
+        void FormatConversion(T obj);
+    }
+
+    interface INoteBookAccessories<out T>
+    {
+        T UpgradeCPU();
+        T UpgradeRAM();
+    }
+}
